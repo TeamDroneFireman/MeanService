b834042233b63e3a016f72750bb3552196d9af12
thomas_huchede <huchede.thomas@gmail.com>
2016-04-25 15:54:46 +0200

    Add user authentication

--- server/models/mean.js
+++ server/models/mean.js
@@ -1,4 +1,18 @@
 module.exports = function(Mean) {
+  const USERSERVICE_URL = 'http://projetm2gla.istic.univ-rennes1.fr:12346/';
+  Mean.beforeRemote('*', function(ctx, unused, next) {
+    Mean.app.datasources.auth
+    .checkAuth(ctx.req.headers.userid, ctx.req.headers.token,
+        function (err, response) {
+      if (err || response.error || response.id !== ctx.req.headers.token) {
+        var e = new Error('You must be logged in to access database');
+        e.status = 401;
+        next(e);
+      } else {
+        next();
+      }
+    });
+  });
   Mean.disableRemoteMethod('deleteById', true);
   Mean.disableRemoteMethod('updateAll', true);
   Mean.disableRemoteMethod('createChangeStream', true);
